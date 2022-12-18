e74e036b53dab79fc2330b2d3f5b1981b945c58d
jiangplus <jiang.plus.times@gmail.com>
2022-12-18 12:02:19 +0800

    add twitter cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com', 'localhost:3000', 'localhost:8080', 'app.sociallayer.im', 'social-layer-app-dev.vercel.app'
+    origins 'example.com', 'localhost:3000', 'localhost:8080', 'app.sociallayer.im', 'social-layer-app-dev.vercel.app', 'twitter.com', 'www.twitter.com', 'm.twitter.com'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
